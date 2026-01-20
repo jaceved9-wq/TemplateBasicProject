c0a211dd9d479aeca6b66e0583b4476d97ff3b3f
A Student <student@nowhere.edu>
2026-01-20 20:48:09 +0000

    Jan 20, 2026, 12:48 PM

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     const double wall = 75;
     const double targetLocation = 150;
     const double targetSpace = 50;
-    const double launchStart = 
+    const double launchStart = 0
 
 
 
