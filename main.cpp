3594196775c0504d8d1528187e554325b53245b9
A Student <student@nowhere.edu>
2026-01-21 01:11:44 +0000

    Jan 20, 2026, 5:11 PM

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     // These are the constant values
-    const double wall = 75;
+    //const double wall = 75;
     const double targetLocation = 150;
     const double targetSpace = 50;
     const double gForce = 32.17405;
@@ -39,7 +39,7 @@ int main() {
     cout << "Height at wall:" << y << "feet." << endl;
     cout << "Possible Range:" << r << "feet." << endl;
     cout << "Target distance:" << targetStart << "-" << targetEnd << "feet." << endl;
-    
+
 
 
 
